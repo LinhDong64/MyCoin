97ccfd0535f77cf2f589b3c565279dc21244115f
LinhDong64 <mailinhdong@gmail.com>
2021-05-15 12:22:18 +0700

    update unspent transactions, check valid structure of txIn, txOut, transaction and check valid address

--- server/source/transaction.js
+++ server/source/transaction.js
@@ -142,7 +142,7 @@ const validateTxIn = (txIn, transaction, aUnspentTxOuts)=>{
         return false;
     }
     const address = referencedUTxOut.address;
-    const key = ec.keyFromPublic(add, 'hex');
+    const key = ec.keyFromPublic(address, 'hex');
     const validSignature = key.verify(transaction.id, txIn.signature);
     if(!validSignature){
         console.log('invalid txIn signature: %s txId: %s address: %s', txIn.signature, transaction.id, referencedUTxOut.address);
@@ -173,3 +173,153 @@ const getCoinbaseTransaction = (address, blockIndex)=>{
     return t;
 }
 exports.getCoinbaseTransaction=getCoinbaseTransaction;
+
+const signTxIn= (transaction, txInIndex, privateKey, aUnspentTxOuts)=>{
+    const txIn = transaction.txIns[txInIndex];
+    const dataToSign = transaction.id;
+    const referencedUnspentTxOut = findUnspentTxOut(txIn.txOutId, txIn.txOutIndex, aUnspentTxOuts);
+    if(referencedUnspentTxOut == null){
+        console.log('Could not find referenced TxOut');
+        throw Error();
+    }
+    const referencedAddress = referencedUnspentTxOut.address;
+    if(getPublicKey(privateKey) !== referencedAddress){
+        console.log('trying to sign an input with private' + 
+        ' key that does not match the address that is referenced in txIn');
+        throw Error();
+    }
+
+    const key= ec.keyFromPrivate(privateKey, 'hex');
+    const signature = toHexString(key.sign(dataToSign).toDER());
+    return signature;
+}
+exports.signTxIn=signTxIn;
+
+const updateUnspentTxOuts = (aTransactions, aUnspentTxOuts)=>{
+    const newUnspentTxOuts = aTransactions.map((t)
+    =>{
+        return t.txOuts.map((txOut, index)=>{
+            return new UnspentTxOut(t.id, index, txOut.address, txOut.amount, )
+        });
+    })
+    .reduce((a,b)=>a.concat(b), []);
+
+    const consumedTxOuts = aTransactions
+    .map((t)=>t.txIns)
+    .reduce((a,b) => a.concat(b), [])
+    .map((txIn)=>new UnspentTxOut(txIn.txOutId, txIn.txOutIndex, '', 0));
+
+    const resultingUnspentTxOuts = aUnspentTxOuts
+    .filter(((uTxO)=>!findUnspentTxOut(uTxO.txOutId, uTxO.txOutIndex, consumedTxOuts)))
+    .concat(newUnspentTxOuts);
+
+    return resultingUnspentTxOuts;
+}
+
+const processTransactions = (aTransactions, aUnspentTxOuts, blockIndex)=>{
+    if(!validateBlockTransactions(aTransactions, aUnspentTxOuts, blockIndex)){
+        console.log('invalid block transactions');
+        return null;
+    }
+    return updateUnspentTxOuts(aTransactions, aUnspentTxOuts);
+}
+exports.processTransactions=processTransactions;
+
+const toHexString = (byteArray)=>{
+    return Array.from(byteArray, (byte)=>{
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join();
+};
+
+const getPublicKey = (aPrivateKey)=>{
+    return ec.keyFromPrivate(aPrivateKey, 'hex').getPublic().encode('hex');
+}
+exports.getPublicKey=getPublicKey;
+
+const isValidTxInStructure = (txIn)=>{
+    if(txIn == null){
+        console.log('txIn is null');
+        return false;
+    }
+    else if (typeof txIn.signature !== 'string') {
+        console.log('invalid signature type in txIn');
+        return false;
+    }
+    else if (typeof txIn.txOutId !== 'string') {
+        console.log('invalid txOutId type in txIn');
+        return false;
+    }
+    else if (typeof txIn.txOutIndex !== 'number') {
+        console.log('invalid txOutIndex type in txIn');
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+const isValidTxOutStructure = (txOut) => {
+    if (txOut == null) {
+        console.log('txOut is null');
+        return false;
+    }
+    else if (typeof txOut.address !== 'string') {
+        console.log('invalid address type in txOut');
+        return false;
+    }
+    else if (!isValidAddress(txOut.address)) {
+        console.log('invalid TxOut address');
+        return false;
+    }
+    else if (typeof txOut.amount !== 'number') {
+        console.log('invalid amount type in txOut');
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+const isValidTransactionStructure = (transaction) => {
+    if (typeof transaction.id !== 'string') {
+        console.log('transactionId missing');
+        return false;
+    }
+    if (!(transaction.txIns instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+    if (!transaction.txIns
+        .map(isValidTxInStructure)
+        .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+    if (!(transaction.txOuts instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+    if (!transaction.txOuts
+        .map(isValidTxOutStructure)
+        .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+    return true;
+};
+
+const isValidAddress = (address) => {
+    if (address.length !== 130) {
+        console.log(address);
+        console.log('invalid public key length');
+        return false;
+    }
+    else if (address.match('^[a-fA-F0-9]+$') === null) {
+        console.log('public key must contain only hex characters');
+        return false;
+    }
+    else if (!address.startsWith('04')) {
+        console.log('public key must start with 04');
+        return false;
+    }
+    return true;
+};
+exports.isValidAddress = isValidAddress;
